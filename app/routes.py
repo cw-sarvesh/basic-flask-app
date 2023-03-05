e7a8b9efc8d8b92243dfe502679d2e6cbbc2fd23
Sarvesh Mishra <sarvesh.m@carwale.com>
2023-03-05 15:35:48 +0530

    update code

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,23 @@
-from flask import Blueprint, render_template_string, render_template
+from flask import Blueprint, render_template, request
 import datetime
 import logging
 import os
 
-
 logger = logging.getLogger(__name__)
 mod_app = Blueprint('mod_app', __name__,
+                    static_folder='static',
                     template_folder=os.path.abspath('templates'))
-
-# TEMP_FOLDER='./templates/'
-# api = Api(mod_app)
-
 
 @mod_app.route("/")
 def home():
     return render_template('index.html', utc_dt=datetime.datetime.now().astimezone())
 
-
-@mod_app.route("/about")
+@mod_app.route("/about",  methods=["GET", "POST"])
 def about():
-    return render_template('about.html', utc_dt=datetime.datetime.now().astimezone())
-
+    if request.method == "GET":
+        return render_template('about.html', utc_dt=datetime.datetime.now().astimezone())
+    else:
+        print(request.form)
 
 @mod_app.route('/comments/')
 def comments():
